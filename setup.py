9029ad952751d88e893e01b9fb2a55df6cfcfa6f
Peter Polidoro <peter@polidoro.io>
2023-01-11 14:36:29 -0500

    Add example code to ros files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'weigher'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='polidorop',
+    maintainer='Peter Polidoro',
     maintainer_email='peter@polidoro.io',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='ROS 2 weigh scale interface. ',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [

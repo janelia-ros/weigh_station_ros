b6f1db10855a6f5a3c7c5d3bdfd3766f23ff0a7b
Peter Polidoro <peter@polidoro.io>
2023-02-14 18:01:46 -0500

    Add weight threshold parameter

--- weigher/launch/weigher_launch.py
+++ weigher/launch/weigher_launch.py
@@ -8,6 +8,9 @@ def generate_launch_description():
     serial_port_arg = DeclareLaunchArgument(
       'serial_port', default_value=TextSubstitution(text='/dev/ttyUSB0')
     )
+    threshold_arg = DeclareLaunchArgument(
+      'threshold', default_value=TextSubstitution(text='100')
+    )
 
     return LaunchDescription([
         serial_port_arg,
@@ -17,6 +20,7 @@ def generate_launch_description():
             name='weigher',
             parameters=[{
             'serial_port': LaunchConfiguration('serial_port'),
+            'threshold': LaunchConfiguration('threshold'),
          }]
         )
     ])
--- weigher/weigher/weigher_node.py
+++ weigher/weigher/weigher_node.py
@@ -16,11 +16,14 @@ class Weigher(Node):
         self.declare_parameter('debug', False)
         debug = self.get_parameter('debug').get_parameter_value().bool_value
         self.declare_parameter('serial_port', '/dev/ttyUSB0')
+        self._threshold = 100
+        self.declare_parameter('threshold', self._threshold)
         self._publisher = self.create_publisher(Weight, 'weight', 10)
         self._dev = LoadstarSensorsInterface(debug=debug)
 
     async def start_getting_sensor_values(self):
         serial_port = self.get_parameter('serial_port').get_parameter_value().string_value
+        self._threshold = self.get_parameter('threshold').get_parameter_value().integer_value
         self.get_logger().info(f'start_getting_sensor_values on serial port: {serial_port}')
         await self._dev.open_high_speed_serial_connection(port=serial_port)
         self._dev.set_sensor_value_units('gram')
@@ -33,10 +36,11 @@ class Weigher(Node):
         await self._dev.stop_getting_sensor_values()
 
     async def sensor_value_callback(self, sensor_value):
-        msg = Weight()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.weight = sensor_value.magnitude
-        self._publisher.publish(msg)
+        if sensor_value.magnitude <= self._threshold:
+            msg = Weight()
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.weight = sensor_value.magnitude
+            self._publisher.publish(msg)
         await asyncio.sleep(0)
 
 
